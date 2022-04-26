ea1a7d450ae95c38055fe2e853ab06b08fe2b9a8
Keri Russin <karussin@student.fullsail.edu>
2022-04-26 17:15:29 -0400

    Updated api/routes/director.js and api/routes/movie.js

--- api/routes/director.js
+++ api/routes/director.js
@@ -9,7 +9,7 @@ const { director } = require("../../messages/messages");
 // GET routes
 router.get("/", (req, res, next) => {
     Director.find({})
-        .select("directorName _id")
+        .select("director _id")
         .exec()
         .then((directorList) => {
             
@@ -39,7 +39,7 @@ router.get("/:directorId", (req, res, next) => {
     const directorId = req.params.directorId;
     
     Director.findById(directorId)
-    .select("directorName _id")
+    .select("director _id")
     .populate("movie", "title")
     .exec()
     .then((director) => {
@@ -68,7 +68,7 @@ router.get("/:directorId", (req, res, next) => {
 
 // POST route
 router.post("/", (req, res, next) => {
-    Director.find({ director: req.body.directorName, movie: req.body.movie, genre: req.body.genre, year: req.body.year })
+    Director.find({ director: req.body.director, movie: req.body.movie, genre: req.body.genre, year: req.body.year })
     .exec()
     .then((result) => {
         
@@ -82,7 +82,7 @@ router.post("/", (req, res, next) => {
         // create director object
         const newDirector = new Director({
             _id: mongoose.Types.ObjectId(),
-            director: req.body.directorName,
+            director: req.body.director,
             movie: req.body.movie,
             genre: req.body.genre,
             year: req.body.year
@@ -116,17 +116,18 @@ router.post("/", (req, res, next) => {
             });
         });
     });
-    
+
 
     // PATCH route
     router.patch("/:directorId", (req, res, next) => {
         const directorId = req.params.directorId;
 
         const updateDirector = {
-            director: req.body.directorName
+            director: req.body.director,
+
         };
 
-        Director.findByIdandUpdate({ _id: directorId }, { $set: updateDirector })
+        Director.findByIdAndUpdate({ _id: directorId }, { $set: updateDirector })
             .exec()
             .then((result) => {
 
@@ -142,7 +143,7 @@ router.post("/", (req, res, next) => {
                 res.status(200).json({
                     message: Messages.director_updated,
                     result: {
-                        director: updateDirector.directorName,
+                        director: updateDirector.director,
                         movie: updateDirector.movie,
                         genre: updateDirector.genre,
                         year: updateDirector.year,
@@ -163,21 +164,22 @@ router.post("/", (req, res, next) => {
             });
     });
 
+
     // DELETE route
     router.delete("/:directorId", (req, res, next) => {
         const directorId = req.params.directorId;
 
         const deleteDirector = {
-            director: req.body.directorName,
+            director: req.body.director,
             movie: req.body.movie,
             genre: req.body.genre,
             year: req.body.year
         };
-        Director.findByIdandDelete({ _id: directorId}, { $set: deleteDirector })
+        Director.findByIdAndDelete({ _id: directorId}, { $set: deleteDirector })
             .exec()
             .then((result) => {
 
-                // validation to check if director exits in collection
+                // validation to check if director exists in collection
                 if(!directorId) {
                     console.log(result);
                     res.status(404).json({
--- api/routes/movie.js
+++ api/routes/movie.js
@@ -2,136 +2,205 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const Movie = require("../models/movie");
+const Messages = require("../../messages/messages");
+const { update } = require("../models/movie");
 
 
 // GET routes
 router.get("/", (req, res, next) => {
     Movie.find({})
-    .then((result) => {
-        res.status(200).json({
-            movie: {
-                title: result.title,
-            },
-        });
-    })
-    .catch((err) => {
-        res.status(500).json({
-            error: {
-                message: err.message,
-            },
-        });
-    });
-});
+        .select("title director _id")
+        .exec()
+        .then((movieList) => {
 
-// POST
-router.post("/", (req, res, next) => {
-    const newMovie = new Movie({
-        _id: mongoose.Types.ObjectId(),
-        title: req.body.title,
-        director: req.body.director
-    });
-
-    // write to the DB
-    newMovie.save()
-    .then (result => {
-        console.log(result);
-        res.status(200).json({
-            message: "Movie Saved",
-            movie:{
-                title: result.title,
-                director: result.director,
-                id: result._id,
-                metadata:{
-                    method: req.method,
-                    host: req.hostname
+            // validation to check if collection is empty
+            if(movieList <1) {
+                res.status(200).json({
+                    message: Messages.movie_collection_empty,
+                });
+            }
+            res.status(200).json({
+                message: Messages.movie_collection,
+                movieList,
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: {
+                    message: err.message,
                 },
-            },
+            });
         });
-    })
-    .catch(err => {
-        console.error(err.message);
-        res.status(500).json({
-            error:{
-                message: err.message
-            },
-        });
-    });
 });
 
-// GET by id
+
 router.get("/:movieId", (req, res, next) => {
     const movieId = req.params.movieId;
-    
+
     Movie.findById({ _id: movieId })
-    .then((result) => {
-        res.status(200).json({
-            message: "Movie Selected",
-            movie: {
-                title: result.title,
-                director: result.director,
-                id: result._id,
-            },
+        .select("title director _id")
+        .exec()
+        .then((movie) => {
+            console.log(movie);
+
+            // validation to check if movie is in collection
+            if(!movie) {
+                res.status(404).json({
+                    message: Messages.movie_not_found,
+                });
+            }
+            res.status(200).json({
+                message: Messages.movie_selected,
+                movie
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: {
+                    message: err.message,
+                },
+            });
         });
-    });
 });
 
-// PATCH by id
+
+// POST route
+router.post("/", (req, res, next) => {
+    Movie.find({ movie: req.body.movie, director: req.body.director, genre: req.body.genre, year: req.body.year })
+        .exec()
+        .then((result) => {
+            console.log(result);
+
+            // validation to check if movie is already in database
+            if(result.length > 0) {
+                return res.status(409).json({
+                    message: Messages.movie_post_duplicated,
+                });
+            }
+
+            // create movie object
+            const newMovie = new Movie({
+                _id: mongoose.Types.ObjectId(),
+                movie: req.body.movie,
+                director: req.body.director,
+            });
+
+            // write new movie info to the database
+            newMovie
+                .save()
+                .then((movie) => {
+                    console.log(movie);
+                    res.status(201).json({
+                        message: Messages.movie_submitted,
+                        movie: {
+                            movie: movie.movie,
+                            director: movie.director,
+                            genre: movie.genre,
+                            year: movie.year,
+                            id: movie._id,
+                        },
+                        metadata: {
+                            method: req.method,
+                            host: req.hostname,
+                        },
+                    });
+                });
+        })
+        .catch((err) => {
+            console.error(err.message);
+            res.status(500).json({
+                error: {
+                    message: err.message,
+                },
+            });
+        });
+});
+
+
+// PATCH route
 router.patch("/:movieId", (req, res, next) => {
     const movieId = req.params.movieId;
+
     const updateMovie = {
-        title: req.body.title,
-        director: req.body.director
+        movie: req.body.movie,
+        director: req.body.director,
+        genre: req.body.genre,
+        year: req.body.year,
     };
-    Movie.updateOne({
-        _id: movieId
-    }, 
-    {
-        $set: updateMovie
-    })
-    .then(result => {
-        res.status(200).json({
-            message: "Updated Movie",
-            movie: {
-                title: result.title,
-                director: result.director,
-                id: result._id
-            },
-            metadata: {
-                host: req.hostname,
-                method: req.method
-            },
+
+    Movie.findByIdAndUpdate({ _id: movieId}, { $set: updateMovie })
+        .exec()
+        .then((result) => {
+
+            // validation to check if movie is in collection
+            if(!movieId) {
+                console.log(result);
+                res.status(404).json({
+                    error: {
+                        message: Messages.movie_not_found,
+                    },
+                });
+            }
+            res.status(200).json({
+                message: Messages.movie_updated,
+                result: {
+                    movie: updateMovie.movie,
+                    director: updateDirector.director,
+                    genre: updateMovie.genre,
+                    year: updateMovie.year,
+                    id: movieId,
+                },
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: {
+                    message: err.message,
+                },
+            });
         });
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: {
-                message: err.message
-            },
-        });    
-    });
 });
 
-// DELETE by id
+
+// DELETE route
 router.delete("/:movieId", (req, res, next) => {
     const movieId = req.params.movieId;
-    Movie.deleteOne({ _id: movieId })
-    .then((result) => {
-        res.status(200).json({
-            message: "Movie Deleted",
-            movie: {
-                title: result.title,
-                director: result.director,
-                id: result._id,
-            },
+
+    const deleteMovie = {
+        movie: req.body.movie,
+        director: req.body.director,
+        genre: req.body.genre,
+        year: req.body.year,
+    };
+
+    Movie.findByIdAndDelete({ _id: movieId }, { $set: deleteMovie })
+        .exec()
+        .then((result) => {
+
+            // validation to check if movie exists in collection
+            if(!movieId) {
+                console.log(result);
+                res.status(404).json({
+                    error: {
+                        message: Messages.movie_not_found,
+                    },
+                });
+            }
+            res.status(200).json({
+                message: Messages.movie_deleted,
+                result,
+                request: {
+                    url: `http://localhost:3000/movie/${movieId}`,
+                },
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: {
+                    message: err.message,
+                },
+            });
         });
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: {
-                message: err.message
-            },
-        });    
-    })
 });
 
 
