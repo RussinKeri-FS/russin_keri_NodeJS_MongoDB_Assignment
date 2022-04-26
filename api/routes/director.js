bf45e4decb63c3a716a1470894f737cbf5781e7e
Keri Russin <karussin@student.fullsail.edu>
2022-04-26 16:27:21 -0400

    Updated info in api/routes/director.js

--- api/routes/director.js
+++ api/routes/director.js
@@ -1,45 +1,209 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+const Director = require("../models/director");
+const Messages = require("../../messages/messages");
+const { director } = require("../../messages/messages");
+
 
 // GET routes
 router.get("/", (req, res, next) => {
-    res.json({
-        message: "Director - GET"
-    });
+    Director.find({})
+        .select("directorName _id")
+        .exec()
+        .then((directorList) => {
+            
+            // validation to check if collection is empty
+            if (directorList <1) {
+                res.status(200).json({
+                    message: Messages.director_empty,
+                });
+            }
+            res.status(200).json({
+                message: Messages.director,
+                directorList,
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: {
+                    message: err.message,
+                },
+            });
+        });
 });
 
-// POST
-router.post("/", (req, res, next) => {
-    res.json({
-        message: "Director - POST"
-    });
-});
 
-// GET by id
+// GET  director by id
 router.get("/:directorId", (req, res, next) => {
     const directorId = req.params.directorId;
-    res.json({
-        message: "Director - GET",
-        id: directorId
+    
+    Director.findById(directorId)
+    .select("directorName _id")
+    .populate("movie", "title")
+    .exec()
+    .then((director) => {
+        
+        // validation to check if director is in the collection
+        if (!director) {
+            console.log(director);
+            return res.status(404).json({
+                message: Messages.director_not_found,
+            });
+        }
+        res.status(201).json({
+            message: Messages.director_found,
+            director,
+        });
+    })
+    .catch((err) => {
+        res.status(500).json({
+            error: {
+                message: err.message,
+            },
+        });
     });
 });
 
-// PATCH by id
-router.patch("/:directorId", (req, res, next) => {
-    const directorId = req.params.directorId;
-    res.json({
-        message: "Director - PATCH",
-        id: directorId
+
+// POST route
+router.post("/", (req, res, next) => {
+    Director.find({ director: req.body.directorName, movie: req.body.movie, genre: req.body.genre, year: req.body.year })
+    .exec()
+    .then((result) => {
+        
+        // validation to check if director is already in database
+        if(result.length > 0) {
+            return res.status(409).json({
+                message: Messages.director_post_duplicated,
+            });
+        }
+        
+        // create director object
+        const newDirector = new Director({
+            _id: mongoose.Types.ObjectId(),
+            director: req.body.directorName,
+            movie: req.body.movie,
+            genre: req.body.genre,
+            year: req.body.year
+        });
+        
+        // write new director info to the database
+        newDirector
+        .save()
+        .then((result) => {
+            console.log(result);
+            res.status(201).json({
+                message: Messages.director_submitted,
+                director: {
+                    director: result.director,
+                    movie: result.movie,
+                    genre: result.genre,
+                    id: result._id,
+                },
+                metatdata: {
+                    method: req.method,
+                    host: req.hostname,
+                },
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                error: {
+                    message: Messages.director_post_error
+                },
+            });
+        });
+    });
+    
+
+    // PATCH route
+    router.patch("/:directorId", (req, res, next) => {
+        const directorId = req.params.directorId;
+
+        const updateDirector = {
+            director: req.body.directorName
+        };
+
+        Director.findByIdandUpdate({ _id: directorId }, { $set: updateDirector })
+            .exec()
+            .then((result) => {
+
+                // validation to check if director is in collection
+                if(!directorId) {
+                    console.log(result);
+                    res.status(404).json({
+                        error: {
+                            message: Messages.director_not_found,
+                        },
+                    });
+                }
+                res.status(200).json({
+                    message: Messages.director_updated,
+                    result: {
+                        director: updateDirector.directorName,
+                        movie: updateDirector.movie,
+                        genre: updateDirector.genre,
+                        year: updateDirector.year,
+                        id: directorId,
+                    },
+                    metadata: {
+                        method: req.method,
+                        host: req.hostname,
+                    },
+                });
+            })
+            .catch((err) => {
+                res.status(500).json({
+                    error: {
+                        message: err.message,
+                    },
+                });
+            });
+    });
+
+    // DELETE route
+    router.delete("/:directorId", (req, res, next) => {
+        const directorId = req.params.directorId;
+
+        const deleteDirector = {
+            director: req.body.directorName,
+            movie: req.body.movie,
+            genre: req.body.genre,
+            year: req.body.year
+        };
+        Director.findByIdandDelete({ _id: directorId}, { $set: deleteDirector })
+            .exec()
+            .then((result) => {
+
+                // validation to check if director exits in collection
+                if(!directorId) {
+                    console.log(result);
+                    res.status(404).json({
+                        error: {
+                            message: Messages.director_not_found,
+                        },
+                    });
+                }
+                res.status(200).json({
+                    message: Messages.director_deleted,
+                    result,
+                    request: {
+                        method: "GET",
+                        url: `http://localhost:3000/director/${directorId}`,
+                    },
+                });
+            })
+            .catch((err) => {
+                res.status(500).json({
+                    error: {
+                        message: err.message,
+                    },
+                });
+            });
     });
 });
 
-// DELETE by id
-router.delete("/:directorId", (req, res, next) => {
-    const directorId = req.params.directorId;
-    res.json({
-        message: "Director - DELETE",
-        id: directorId
-    });
-});
 
 module.exports = router;
