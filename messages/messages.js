c53dabc224671092eab58db9aaa2cbfc41f0b36e
Keri Russin <karussin@student.fullsail.edu>
2022-04-26 15:22:17 -0400

    Added a messages.js file

---
+++ messages/messages.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+    // GET
+    movie_collection: "Movie Collection",
+    movie_collection_empty: "You have no movies in your collection. Add a movie to see it in your collection",
+    director: "Directors",
+    director_empty: "You haven't added any directors to your collection. Add director(s) to see them in your collection",
+    movie_selected: "Movie Selected",
+    director_found: "Director Found",
+
+    // POST
+    movie_submitted: "Movie Submitted",
+    director_submitted: "Director Submitted",
+    movie_post_duplicated: "This movie is already in the collection",
+    director_post_duplicated: "This director(s) info is already in the collection",
+    movie_post_error: "Unable to save movie info. Please check and try again",
+    director_post_error: "Unable to save director info. Please check and try again",
+
+    // PATCH
+    movie_updated: "Movie Updated",
+    director_updated: "Director Updated",
+
+    // DELETE
+    movie_deleted: "Movie Deleted",
+    director_deleted: "Director Deleted",
+
+    // 404
+    movie_not_found: "Movie could not be found. Check the ID to make sure it's correct",
+    director_not_found: "Director could not be found. Check the ID to make sure it's correct",
+};
